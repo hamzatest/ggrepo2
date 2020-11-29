806857268565a18330c889d570f0eb66d442f33f
hamzatest <hamzalouar+2@gmail.com>
2020-11-29 12:05:39 +0100

    updated code to trigger gg

--- testgg.js
+++ testgg.js
@@ -2,5 +2,6 @@ const client_secret = 'a57fa13813324da8bff7012814796ef5'
 const secret_api_key = 'ai329732khbfdkg98y342kjbsdf'
 
 function nothingHere() {
-	console.log('nothing here');
+	console.log('nothing here')
+	console.log('update code')
 }
